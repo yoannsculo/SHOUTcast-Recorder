619de6e3f0d37d9d6b9ee5857bb05e7c5cd29970
smitdol <smitdol@brade.nl>
2020-08-28 11:44:39 +0200

    HTTP 1.0

--- src/curl.c
+++ src/curl.c
@@ -39,6 +39,7 @@ int read_stream(Stream *stream)
 	curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
 	curl_easy_setopt(curl, CURLOPT_TCP_KEEPIDLE, 60L);
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
 
 	curl_res = curl_easy_perform(curl);
 	if (curl_res != CURLE_OK && curl_res != CURLE_OPERATION_TIMEDOUT) {
