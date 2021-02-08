f5018147bc29d0af30efdaeb496c5f9298d138cf
smitdol <smitdol@brade.nl>
2021-02-08 19:59:17 +0100

    added timestamp to SwapOfs logline

--- src/curl.c
+++ src/curl.c
@@ -41,7 +41,17 @@ void SwapOfs(void *p) {
      be used */
   if( (curtime - myp->lastruntime) >= duration || difftime(now,myp->last)>= stream->duration) {
     newfilename(stream, stream->stream_title);
-    printf("SwapOfs %lld %lld-%08lld=%lld %ld-%ld=%ld %s\n", duration, curtime, myp->lastruntime,curtime-myp->lastruntime, now, myp->last, now-myp->last,stream->filename);
+
+    struct timeval curTime;
+    gettimeofday(&curTime, NULL);
+    int milli = curTime.tv_usec / 1000;
+
+    char buffr [80];
+    strftime(buffr, 80, "%Y-%m-%d %H:%M:%S", localtime(&curTime.tv_sec));
+    char currentTime[84] = "";
+    sprintf(currentTime, "%s.%03d", buffr, milli);
+
+    printf("%s SwapOfs %lld %lld-%08lld=%lld %ld-%ld=%ld %s\n", currentTime, duration, curtime, myp->lastruntime,curtime-myp->lastruntime, now, myp->last, now-myp->last,stream->filename);
     myp->lastruntime = curtime;
     myp->last=now;
   }
